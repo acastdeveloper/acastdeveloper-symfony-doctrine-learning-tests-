fda7af33bf00fce805f2ad19402de6a9887f4c8f
acastdeveloper <ac.desenvolupador@gmail.com>
2017-06-05 14:00:46 +0200

    Explicacions comentades de cada ordre a PruebasController

--- src/AppBundle/Controller/PruebasController.php
+++ src/AppBundle/Controller/PruebasController.php
@@ -66,30 +66,47 @@ class PruebasController extends Controller
 
     }
 
-
+q
     public function readAction() {
 
       $em =$this->getDoctrine()->getManager();
-      //Obre el gestor d'entitats per a $em
+      //Per gestionar les nostres entitats obrim el getEntityManager o getManager.
 
       $cursos_repo =$em->getRepository("AppBundle:Curso");
-      
-      $cursos =$cursos_repo->findAll();
+      //El repositori és l'objecte que ens permet seleccionar concretament amb quina "entitat" 
+      //treballarem. Aquesta entitat és representant d'una taula a la base de dades.
+
+      $cursos = $cursos_repo->findAll();
+      //$cursos és un array, d'objectes, format per tots els registres de la taula cursos, perquè 
+      //ha fet servir el mètode findAll(). Hi ha altres "find", com find(), findBy(), findOneBy() ,
+      //Obteniendo Datos
+      //Para obtener datos de las tablas tenemos varios métodos realmente mágicos:
+      //findAll(): Obtiene todos los registros de la tabla. Retorna un array.
+      //find(): Obtiene un registro a partir de la clave primaria de la tabla.
+      //findBy(): Obtiene los registros encontrados pudiendo pasar como argumentos los valores que irían dentro del WHERE. Retorna un array.
+      //findOneBy(): obtiene un registro pudiendo pasar como argumentos los valores que irían dentro del WHERE.
+      //Més info: http://www.maestrosdelweb.com/curso-symfony2-manipulando-datos-con-doctrine/
+
+
 
       $curso_ochenta=$cursos_repo->findBy(array("precio"=>80));
+      //$curso_ochenta és un array obtingut pels registres amb "precio" 80.
       //Es el equivalente a un WHERE de mysql
       //Me pasa un array de objetos, donde cada objeto es un registro de la tabla
       echo $curso_ochenta[3]->getTitulo();
+      //M'imprimeix el títol del quart element de l'array format per objectes amb preu 80
       echo count($curso_ochenta);
+      //Imprimeix tots els objectes(=registres) de preu 80.
       echo "<hr>";
 
       echo "<div style='border:1px solid red;padding:20px'>";
       $curso_ochentaB=$cursos_repo->findOneByPrecio(80);
-      //Ens treu el primer que troba
+      //En aquest cas, $curso_ochentaB, no és una matriu. És un únic objecte.
+      //Per això no cal indicar cap índex
+      //Ens treu el primer objecte(registre) que troba amb "precio" 80.
       echo $curso_ochentaB->getTitulo();
       echo "</div>";
       echo "<hr>";
-
 
       foreach($cursos as $curso) {
         echo $curso->getTitulo()."<br>";
@@ -97,23 +114,34 @@ class PruebasController extends Controller
         echo $curso->getPrecio()."<br><hr>";
       }
 
+
       die();
 
     }
 
 
     public function updateAction($id, $titulo, $descripcion, $precio) {
+      //En aquesta funció li passarem els paràmetres: 
+      //$id, $titulo, $descripcion, $precio
 
       $em =$this->getDoctrine()->getEntityManager();
+      //Obre el gestor d'entitats
       $cursos_repo = $em->getRepository("AppBundle:Curso");
+      //Selecciona el repositori amb qui treballarà
 
       $curso = $cursos_repo->find($id);
+      //$curso és un objecte(registre) concret per aquella $id concreta i única 
+
       $curso->setTitulo($titulo);
       $curso->setDescripcion($descripcion);
       $curso->setPrecio($precio);
+      //Estableix nous valors per aquell objecte(registre).
+
 
       $em->persist($curso);
+      //Els guarda
       $flush=$em->flush();
+      //Els aboca a la base de dades
 
       if($flush !=null)  {
         echo "La modificación ha fallado";
@@ -126,14 +154,22 @@ class PruebasController extends Controller
     }
 
     public function deleteAction($id) {
+      //En aquesta funció li passarem el paràmetre $id
 
       $em =$this->getDoctrine()->getEntityManager();
+      //Obre el gestor d'entitats
       $cursos_repo = $em->getRepository("AppBundle:Curso");
+      //Escull el repositori(la taula) amb la que treballarem
 
       $curso = $cursos_repo->find($id);
+      //$curso és l'objecte(registre) del repositori(la taula) 
+      //concret amb la $id que li hem passat
       $em->remove($curso);
+      //Li diem al gestor d'entitats que esborri aquest objecte del repositori
 
       $flush=$em->flush();
+      //Amb aquesta ordre flush en haver esborrat l'objecte del repositori
+      //transmetrà l'ordre a la base de dades perquè esborri el registre corresponent
 
       if($flush!=null) {
         echo "No se ha borrado bien!!";
@@ -147,19 +183,32 @@ class PruebasController extends Controller
 
 
     public function nativeSqlAction() {
+      //EXEMBLE DE FUNCIÓ PER FER CONSULTES AMB SQL "TRADICIONAL"
       $em =$this->getDoctrine()->getEntityManager();
+      //Obre el gestor d'entitats
       $db=$em->getConnection();
+      //Es connecta a la base de dades
 
       $query = 'SELECT * FROM cursos';
+      //Defineix en llenguatge SQL una consulta.
+      //En aquest cas està seleccionant tots els registres de la taula "cursos"
       $stmt = $db->prepare($query);
+      //Prepara la consulta $stmt
       $params = array();
+      //Inicialitza una matriu $params 
+
       $stmt->execute($params);
+      //Executa la consulta $stmt
+
       $cursos = $stmt->fetchAll();
       //Ens tornarà un array d'arrays,
       //en comptes d'un array d'objectes
 
       foreach ($cursos as $curso) {
+       //Amb foreach itera aquest array d'arrays 
         echo $curso["titulo"];
+        //Al ser un array d'arrays per extreure el valor no fa servir 
+        //la fletxa "->" si no els indexos ["titulo"]
         echo "-";
         echo $curso["descripcion"];
         echo "-";
@@ -167,22 +216,37 @@ class PruebasController extends Controller
         echo "<hr>";
       }
 
-
-
       die();
 
     }
 
 
     public function dqlAction() {
+      //EXEMPLE DE FUNCIÓ AMB EL DIALECTE 
+      //DE SQL PROPI DE DOCTRINE, EL DQL
       $em =$this->getDoctrine()->getEntityManager();
+      //Obre el gestor d'entitats
       $query = $em->createQuery("
               SELECT c FROM AppBundle:Curso c
+              /* Observacions:
+              1) No està adreçant-se a la taula de la bbdd si no al repositori.
+              2) Crec que en DQL no posa asterisc * darrera del select sino c, 
+                  perquè és alies que posa a entitat Curso
+               */
               WHERE c.precio > :precio
+              /* Per evitar injeccions d'SQL fa servir lo dels dos punts, igual que amb PDO */
             ")->setParameter("precio","79");
+      //Defineix la consulta amb un mètode de Doctrine anomenat
+      //createQuery()
       $cursos = $query->getResult();
+      //$cursos és la matriu d'objectes obtinguda amb el mètode getResult, 
+      //crec que de DQL, que obté els resultats de la consulta en forma 
+      //de matriu d'objectes
+      
       foreach ($cursos as $curso) {
+      //Itera la matriu d'objectes(registres) $cursos i a cada ítem l'anomena $curso
         echo $curso->getTitulo()."<br>";
+        //Imprimeix de cada objecte $curso la seva propietat título
       }
       die();
     }
@@ -192,7 +256,9 @@ class PruebasController extends Controller
 
     public function queryBuilderAction() {
       $em =$this->getDoctrine()->getEntityManager();
+      //Obre el gestor d'entitats
       $cursos_repo = $em->getRepository("AppBundle:Curso");
+      //Accedeix al repositori/taula Curso.
 
       $query = $cursos_repo->createQueryBuilder("c")
               ->where("c.precio > :precio")
